40709152dfde7136d6f579e5bd0cfed3dbf668ab
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-01-13 18:16:10 -0300

    .

---
+++ Website001/index.js
@@ -0,0 +1,11 @@
+let Fullname = "Lucas Pereira"
+let age = 18
+let Isstudent = true
+
+document.getElementById("p1").textContent = `Your name is ${Fullname}`
+
+document.getElementById("p2").textContent = `You are ${age} years old`
+
+document.getElementById("p3").textContent = `Enrolled: ${Isstudent}`
+
+
