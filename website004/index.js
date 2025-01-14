ddddf07041f312058d7e53a5915792f1d26bfabd
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-01-14 18:14:50 -0300

    .

---
+++ website004/index.js
@@ -0,0 +1,16 @@
+// Type conversion = change the datatype of a value to another
+//                                      (strings, numbers, booleans)
+
+let x = "pizza"
+let y = "pizza"
+let z = "pizza"
+
+x = Number(x)
+y = String(y)
+z = Boolean(z)
+
+console.log(x, typeof x)
+console.log(y, typeof y)
+console.log(z, typeof z)
+
+// String não pode ser transformada em número mas número pode ser transformado em string , Boolean pode sempre ser mudada pra qualquer
