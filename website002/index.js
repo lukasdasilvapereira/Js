318c9261ccca49354536b054fa939e2f7e5d78dc
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-01-14 17:46:34 -0300

    .

---
+++ website002/index.js
@@ -0,0 +1,30 @@
+let students = 30
+
+//students = students + 1
+//students = students - 1
+//students = students * 2
+//students = students / 2
+//students = students % 3
+//students = students ** 2
+
+//students++
+//students--
+//students *= 2
+//students /= 2
+//students %= 3
+//students **= 2
+
+ 
+//console.log(students)
+
+/* Ordem de aparição 
+
+1- Parenteses()
+2- Exponenciação**
+3- Multiplicação *, divisão / , modulo %
+4- Adição +, subtração -
+*/
+
+let result = 1 * 2 / 10 - 2 + 5
+
+console.log(result)
