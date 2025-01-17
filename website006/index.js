924da20477d91a4c676015edf554ad34334199cc
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-01-17 18:22:58 -0300

    .

---
+++ website006/index.js
@@ -0,0 +1,19 @@
+// Ternary , Um uso alternativo de if e else
+
+//let age = 15
+
+//let message = age >= 18 ? "You are an adult" : "You are NOT an adult"
+
+//console.log(message)
+
+// let time = 9
+
+// let message = time >= 13? "Good afternoon" : "Good morning"
+
+// console.log(message)
+
+let isStudent = false
+
+let message = isStudent ? "You are enrolled" : "You are not enrolled";
+
+console.log(message)
