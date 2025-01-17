d1486e966ad6a9bdc395f8fe055c5e912ebf2748
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-01-17 18:55:17 -0300

    .

---
+++ website008/index.js
@@ -0,0 +1,33 @@
+// String methods , So many ways to manipulate your string
+
+let username = "Lucas"
+
+console.log(username.charAt(0))
+
+console.log(username.indexOf("u"))
+
+console.log(username.lastIndexOf("u"))
+
+console.log(username.length)
+
+console.log(username.trim())
+
+console.log(username.toLowerCase())
+
+console.log(username.toUpperCase())
+
+console.log(username.replaceAll("u", "o"))
+
+console.log(username.replace("u", "o"))
+
+console.log(username.startsWith("u"))
+
+console.log(username.endsWith("s"))
+
+console.log(username.includes("u"))
+
+console.log(username.repeat(3))
+
+console.log(username.padStart(20, "0"))
+
+console.log(username.padEnd(20, "0"))
