c88728650c4d566d3b538062746bf89e84e6e0b8
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-01-18 18:45:11 -0300

    .

---
+++ website011/index.js
@@ -0,0 +1,23 @@
+// = assignament operator
+// == comparison operator (compare if value are equal)
+// === strict equality operator ( compare if values & datatype are equal)
+// != inequality operator 
+// !== strict inequality operator
+
+const PI = 3.14
+
+const num = 10
+
+if (PI === "3.14") {
+    console.log("The number is equal to PI")
+} else {
+    console.log("The number is not equal to PI")
+}
+
+if (num !== 20) {
+    console.log("The number is different to 10")
+} 
+else {
+    console.log("The number is equal to 10")
+}
+
