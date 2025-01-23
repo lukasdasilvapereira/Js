0649889ac5c06a72049e1c5ecdc78ee6e38377e1
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-01-23 18:29:10 -0300

    .

---
+++ website016/index.js
@@ -0,0 +1,13 @@
+// ARRAYS
+
+let fruits = ["apple", "banana", "orange", "grape"];
+
+fruits.push("cocunut") // Isso adiciona um elemento a mais
+fruits.pop() // Isso tira o ultimo elemento colocado
+fruits.sort() // Organiza em ordem alfabetica
+fruits.sort().reverse() // Deixa em ordem ao contrario
+
+for(let fruit of fruits) {
+   console.log(fruits);
+}
+
